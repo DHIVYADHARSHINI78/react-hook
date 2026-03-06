9155e1c56adbc866d3cc01ed094fc909522bcf36
DHIVYADHARSHINI78 <dhivyak463@gmail.com>
2026-03-06 17:31:38 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-// Entry point
-// Run: npm install  →  npm start
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'antd/dist/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
